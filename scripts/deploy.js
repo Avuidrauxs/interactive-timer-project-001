66f52171a29f259cb0deb190f351393604d7e19e
tepeszCodes <tomek.szymanderski@gmail.com>
2021-07-21 17:33:12 +0200

    simplified deploy.js script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,19 +2,4 @@ const simpleGit = require('simple-git');
 
 const git = simpleGit();
 
-/**
- * placeholder
- * @returns {int} res
- * @param {int} res
- */
-function onPush(res) {
-  switch (res) {
-    case null:
-      console.log('Everything up to date');
-      break;
-    default:
-      console.log(res);
-  }
-}
-
-git.push('heroku', `${process.argv[2]}:main`, onPush).catch((e) => console.log(e));
+git.push('heroku', process.argv[2] !== undefined ? `${process.argv[2]}:main` : 'main', (res) => console.log(res)).catch((e) => console.log(e));
