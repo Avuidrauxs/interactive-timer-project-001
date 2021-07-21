9c64aaafc016c5b15e2342dfcdc33972341b963a
tepeszCodes <tomek.szymanderski@gmail.com>
2021-07-21 23:00:42 +0200

    added SpeedBar component and a helper CustomButton component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap';
 import InputBar from './Components/InputBar';
+import SpeedBar from './Components/SpeedBar';
 
 /** This function renders the entire app
  * @returns {Node} containing the app
  */
 function App() {
   return (
-    <Row md={3}>
-      <InputBar />
-    </Row>
+    <Col xs={12} md={6}>
+      <Row>
+        <InputBar />
+      </Row>
+      <Row>
+        <SpeedBar />
+      </Row>
+    </Col>
   );
 }
 
