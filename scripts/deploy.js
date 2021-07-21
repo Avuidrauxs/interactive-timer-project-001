f1503432b84ea5b1452ab4c882e9cb6aad3679c2
tepeszCodes <tomek.szymanderski@gmail.com>
2021-07-21 17:24:28 +0200

    fixed errors linted by ESLint

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,26 +1,20 @@
 const simpleGit = require('simple-git');
+
 const git = simpleGit();
 
-console.log('heroku', `${process.argv[2]}:main`)
-
+/**
+ * placeholder
+ * @returns {int} res
+ * @param {int} res
+ */
 function onPush(res) {
-    switch(res) {
-        case null:
-            console.log('Everything up to date');
-            break;
-        default:
-            console.log(res);
-    }
+  switch (res) {
+    case null:
+      console.log('Everything up to date');
+      break;
+    default:
+      console.log(res);
+  }
 }
 
-// async function pushBranch() {
-//     try {
-//         await git.push('heroku', `${process.argv[2]}:main`, onPush)
-//     } catch(error) {
-//         console.log(error)
-//     }
-// }
-
-// pushBranch();
-
-git.push('heroku', `${process.argv[2]}:main`, onPush).catch((e) => console.log(e))
+git.push('heroku', `${process.argv[2]}:main`, onPush).catch((e) => console.log(e));
