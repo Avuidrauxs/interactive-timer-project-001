7a187ef83eb0b509ea06d0651d4935ffe4e3536e
tepeszCodes <54599709+tepeszCodes@users.noreply.github.com>
2021-07-23 20:06:12 +0200

    Update Clock.js

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -7,7 +7,7 @@ function Clock() {
   const clockStyle = { fontSize: '15vw' };
   return (
     <Row xs={1} className="justify-content-center m-auto my-0">
-      <p className="text-center fw-bold my-0" style={{ fontSize: '15vw' }}>23:12</p>
+      <p className="text-center fw-bold my-0" style={clockStyle}>23:12</p>
     </Row>
   );
 }
