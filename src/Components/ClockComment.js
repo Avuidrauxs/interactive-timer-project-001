96b15ad9b3d0dcdc55aa31a96786b1eb06f13073
tepeszCodes <54599709+tepeszCodes@users.noreply.github.com>
2021-07-23 20:43:19 +0200

    Update ClockComment.js

--- src/Components/ClockComment.js
+++ src/Components/ClockComment.js
@@ -3,7 +3,7 @@ import { Row } from 'react-bootstrap';
 /** function generating the text to be displayed above the clock
  * @returns {Node} representing component
  */
-function ClockComment() {
+function ClockComment(props) {
   return (
     <Row xs={1} className="justify-content-center m-auto">
       <p className="fs-5 fst-italic text-center">{props.text}</p>
