564a84e637776649ad02cc8f7e32419ad1016cf7
tepeszCodes <tomek.szymanderski@gmail.com>
2021-07-24 15:26:25 +0200

    Added interface to enter time and input validation. Used React Context as a proof of concept

--- src/Components/InputBar.js
+++ src/Components/InputBar.js
@@ -1,17 +1,68 @@
-import { Form, Row, Button } from 'react-bootstrap';
+/* eslint-disable max-len */
+import { useState, useContext } from 'react';
+// eslint-disable-next-line object-curly-newline
+import { Form, Row, Button, Alert } from 'react-bootstrap';
+import TimerContext from '../Contexts/TimerContext';
 
 /** function generating an input bar
  * @returns {Node} representing the input bar
  */
 function InputBar() {
+  const [input, setInput] = useState('');
+  const [showWarning, setShowWarning] = useState(false);
+  const { setUserInput } = useContext(TimerContext);
+  /** function validating the user input.
+   * @param {string} value represents user input
+   * @returns {boolean} informing if the input is valid
+  */
+  function validate(value) {
+    if (value === '') {
+      return false;
+    }
+    const numValue = parseInt(value, 10);
+    if (Number.isNaN(numValue) || numValue < 0 || numValue > 1380) {
+      return false;
+    }
+    return true;
+  }
+  /** function handling the click and setting the current
+   * value of user input as userInput in global context
+   * @returns {null}. */
+  function submit() {
+    const isValid = validate(input);
+    if (isValid) {
+      setShowWarning(false);
+      setUserInput(input);
+    } else {
+      setShowWarning(true);
+    }
+  }
   return (
-    <Row xs={1} className="justify-content-center m-auto">
-      <Form className="d-flex flex-row align-items-center">
-        <Form.Label className="my-0 fw-bold">Countdown:</Form.Label>
-        <Form.Control className="mx-2 rounded-0 border-2 border-secondary" type="input" placeholder="(Min)" />
-        <Button className="rounded-0 bg-success border-0">START</Button>
-      </Form>
-    </Row>
+    <>
+      <Row xs={1} className="justify-content-center m-auto">
+        <Form className="d-flex flex-row align-items-center">
+          <Form.Label className="my-0 fw-bold">Countdown:</Form.Label>
+          <Form.Control
+            className="mx-2 rounded-0 border-2 border-secondary"
+            type="input"
+            placeholder="(Min)"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <Button
+            className="rounded-0 bg-success border-0"
+            onClick={submit}
+          >
+            START
+          </Button>
+        </Form>
+      </Row>
+      {showWarning
+        && <Alert variant="warning" className="mt-2">
+          Your input is invalid. Input field must not be empty and you can only input numbers between 0 and 1380 (1380 minutes = 23 hours).
+        </Alert>
+      }
+    </>
   );
 }
 
