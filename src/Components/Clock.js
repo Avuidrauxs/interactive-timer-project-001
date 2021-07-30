f7735f311b051898ddbb1135106c275b4fd02482
tepeszCodes <tomek.szymanderski@gmail.com>
2021-07-30 12:05:49 +0200

    used ReactFitty package to make clock display always be the right size

--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -1,5 +1,6 @@
 import { Row } from 'react-bootstrap';
 import { useContext } from 'react';
+import { ReactFitty } from 'react-fitty';
 import TimerContext from '../Contexts/TimerContext';
 
 /** function generating a timer clock
@@ -7,17 +8,16 @@ import TimerContext from '../Contexts/TimerContext';
  */
 function Clock() {
   const { timeLeft } = useContext(TimerContext);
-  const clockStyle = { fontSize: '10vw' };
   const hours = Math.floor(timeLeft / 3600);
   const minutes = Math.floor((timeLeft % 3600) / 60);
   const seconds = timeLeft % 60;
   return (
     <Row xs={1} className="justify-content-center m-auto my-0">
-      <p className="text-center fw-bold my-0" style={clockStyle}>
+      <ReactFitty>
         {hours < 10 && 0}{hours}:
         {minutes < 10 && 0}{minutes}:
         {seconds < 10 && 0}{seconds}
-      </p>
+      </ReactFitty>
     </Row>
   );
 }
